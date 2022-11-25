3d4d30d4a2a59946bde60cf0e42e92f952e6c927
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2022-11-25 02:48:02 +0200

    Update SettingsMenuInterface.h

--- SettingsMenuInterface.h
+++ SettingsMenuInterface.h
@@ -8,12 +8,26 @@
 
 namespace SettingsMenuInterface
 {
+    static const int SETTINGS_WIDTH = 600;
+    static const int SETTINGS_HEIGHT = 400;
+    static const char* SETTINGS_TITLE = "Settings";
+    static const int BACKGROUND_COLOR = RGB(200, 200, 200);
+
     class RectangleButton {
+    public:
+        RectangleButton();
         RectangleButton(const int& xCenter, const int& yCenter, const int& xLen, const int& yLen);
         void drawButton(const int& color);
         bool isClicked(const int& x, const int& y) const;
+    private:
+        int m_xCenter;
+        int m_yCenter;
+        int m_xLen;
+        int m_yLen;
     };
-    class FlagButton;
+    class FlagButton : RectangleButton {
+
+    };
    // class ScrollableDropdownList;
 
     void run();
