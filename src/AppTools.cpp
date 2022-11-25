19aeb991e95ee5bb899beea79b3873dac1d9cf89
sab <87321707+citrtoj@users.noreply.github.com>
2022-11-25 22:13:58 +0200

    Added unfinished mockup menu, not functional

--- src/AppTools.cpp
+++ src/AppTools.cpp
@@ -1,15 +1,15 @@
 #include "AppTools.h"
-
+#include<iostream>
 Label::Label() :
     xCenter(), yCenter(), xLen(), yLen(), fillColor() {}
 
 Label::Label(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_) :
-    xCenter(xCenter_), yCenter(yCenter_), xLen(xLen_), yLen(yLen_), fillColor(fillColor_) {}
+    xCenter(xCenter_), yCenter(yCenter_), xLen(xLen_), yLen(yLen_), fillColor(fillColor_){}
 
 void Label::drawLabel(const int& outlineColor) {
     setcolor(outlineColor);
     rectangle(xCenter - xLen / 2, yCenter - yLen / 2,
-              xCenter + xLen / 2, yCenter + yLen / 2);
+        xCenter + xLen / 2, yCenter + yLen / 2);
     if (fillColor >= 0) {
         setfillstyle(SOLID_FILL, fillColor);
         bar(xCenter - xLen / 2 + 1, yCenter - yLen / 2 + 1,
@@ -23,6 +23,24 @@ void Label::clear(const int& barColor) {
         xCenter + xLen / 2, yCenter + yLen / 2);
 }
 
+TextLabel::TextLabel() {
+    m_text = "";
+}
+TextLabel::TextLabel(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_, const char*& text) :
+    Label(xCenter_, yCenter_, xLen_, yLen_, fillColor_), m_text(text){}
+
+void TextLabel::drawText(const int& txtFont, const int& txtSize) {
+    settextstyle(txtFont, 0, txtSize);
+    setbkcolor(fillColor);
+    outtextxy(xCenter - textwidth(m_text.data()) / 2, yCenter - textheight(m_text.data()) / 2, m_text.data());
+}
+
+void TextLabel::drawTextLabel(const int& txtFont, const int& txtSize) {
+    drawLabel();
+    drawText(txtFont, txtSize);
+}
+
+
 Button::Button() = default;
 
 Button::Button(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_) :
@@ -30,14 +48,31 @@ Button::Button(const int& xCenter_, const int& yCenter_, const int& xLen_, const
 
 bool Button::hitCollision(const int& x, const int& y) {
     return xCenter - xLen / 2 <= x && x <= xCenter + xLen / 2 &&
-            yCenter - yLen / 2 <= y && y <= yCenter + yLen / 2;
+        yCenter - yLen / 2 <= y && y <= yCenter + yLen / 2;
 }
+
+
+ImageButton::ImageButton() {
+    filename = "";
+}
+ImageButton::ImageButton(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fC_, const char*& filename_) :
+    Button(xCenter_, yCenter_, xLen_, yLen_, fC_), filename(filename_) {}
+
+void ImageButton::drawImageButton() {
+
+    readimagefile(filename.data(), xCenter - xLen / 2, yCenter - yLen / 2, xCenter + xLen / 2, yCenter + yLen / 2);
+    rectangle(xCenter - xLen / 2, yCenter - yLen / 2,
+        xCenter + xLen / 2, yCenter + yLen / 2);
+
+
+}
+
 
 TextButton::TextButton() :
     Button(), m_text() {}
 
 TextButton::TextButton(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_,
-                       const int& fillColor_, const char* p) :
+    const int& fillColor_, const char* p) :
     Button(xCenter_, yCenter_, xLen_, yLen_, fillColor_), m_text(p) {}
 
 void TextButton::drawText(const int& txtFont, const int& txtSize) {
--- src/SettingsMenuInterface.cpp
+++ src/SettingsMenuInterface.cpp
@@ -1,110 +1,83 @@
-#include "SettingsMenuInterface.h"
+#include "../include/SettingsMenuInterface.h"
 #include<iostream>
-#include "graphics.h"
-#include "winbgim.h"
-
+#include"../include/AppTools.h"
 using namespace SettingsMenuInterface;
 
-RectangleButton::RectangleButton() = default;
-
-RectangleButton::RectangleButton(const int& xCenter, const int& yCenter, const int& xLen, const int& yLen) :
-    m_xCenter(xCenter), m_yCenter(yCenter), m_xLen(xLen), m_yLen(yLen) {}
-
-void RectangleButton::drawButton(const int& color = BLUE) {
-    setfillstyle(SOLID_FILL, color);
-    bar(m_xCenter - m_xLen / 2, m_yCenter - m_yLen / 2,
-              m_xCenter + m_xLen / 2, m_yCenter + m_yLen / 2);
-}
-
-bool RectangleButton::isClicked(const int& x, const int& y) const {
-    return m_xCenter - m_xLen / 2 <= x && m_xCenter + m_xLen / 2 >= x &&
-            m_yCenter - m_yLen / 2 <= y && m_yCenter + m_yLen / 2 >= y;
-}
-TextButton::TextButton() = default;
-
-TextButton::TextButton(const int& xCenter, const int& yCenter, const int& xLen, const int& yLen, const MyArray<char,256>& text):
-    RectangleButton(xCenter,yCenter,xLen,yLen), m_text(text){}
-
-void TextButton::drawTextButton() {
-    drawButton(BUTTON_COLOR);
-    setbkcolor(BUTTON_COLOR);
-    setcolor(TEXT_COLOR);
-    outtextxy(m_xCenter, m_yCenter,m_text.data());
-}
-
-TextLabel::TextLabel() = default;
-TextLabel::TextLabel(const int& xCenter, const int& yCenter, const int& xLen, const int& yLen, const MyArray<char, 256>& text):
-    m_xCenter(xCenter), m_yCenter(yCenter), m_xLen(xLen), m_yLen(yLen), m_text(text){}
-
-void TextLabel::drawTextLabel() {
-    setfillstyle(SOLID_FILL, LABEL_COLOR);
-    bar(m_xCenter - m_xLen / 2, m_yCenter - m_yLen / 2,
-        m_xCenter + m_xLen / 2, m_yCenter + m_yLen / 2);
-
-    setbkcolor(LABEL_COLOR);
-    setcolor(TEXT_COLOR);
-    outtextxy(m_xCenter, m_yCenter, m_text.data());
-
-}
 
 void SettingsMenuInterface::drawScreen() {
     setfillstyle(SOLID_FILL, BACKGROUND_COLOR);
     bar(0, 0, getmaxx(), getmaxy());
-
 }
 
 void SettingsMenuInterface::setTextSettings() {
-    settextstyle(0, 0, 2);
-    settextjustify(1, 1);
-}
+    settextstyle(FONT, 0, FONT_SIZE);
 
-void SettingsMenuInterface::checkClick() {
-    /*
+}
+template<class T>void SettingsMenuInterface::checkClick(MyArray<T,16> arr) {
+
     if (ismouseclick(WM_LBUTTONDOWN))
         {
             int x, y;
             getmouseclick(WM_LBUTTONDOWN, x, y);
-            for (size_t i = 0; i < rectButtons.size(); i++) {
-                if (rectButtons[i].isClicked(x,y)) {
-                    std::cout << i << " " << x << " " << y<<"\n";
+            for (size_t i = 0; i < arr.size(); i++) {
+                /*if (arr[i].isClicked(x,y)) {
+                    std::cout << i << " " << x << " " << y<<"\n";*/
+                    //tre sa vad cum fac cu isClicked ca tu ai schimbat in collision am vazut.
+                    //idee: vreau sa fac templateul sa verifice daca i array de Dropdowns
+                    //si da, cred ca am sa fac un array pentru singurul dropdown ever
+                    //sau... pot sa fac pentru un singur element? dar daca tot ii template facut pt arrayuri...
+                    //plus poate mai adaugam setari
+                    //iar collisionul dropdownului sa returneze int (indexul elementului clickuit)
+                    //si nu doar bool
+                    //ca sa schimbam doar un parametru cu care vom apela o functie
+                    //eu asa consider
                 }
-            }
+
         }
-    */
-    //o mut aici pentru referinta
-    //dar gen
-    //creca trebe oricum sa fie un template function
-    //DACA as putea sa fac myarray<myarrayuri> cu tipuri diferite, poate ar fi mai ok idk
-    // totusi cred ca o sa hardcodam un picut clasele astea cu textButton si asa
-    // pentru ca as vrea sa fie undeva o functie apelata care in functie de indexul elementului
-    // apeleaza o alta functie (ce ziceai tu cu ifuri si elseuri)
+
+
 }
 
 void SettingsMenuInterface::run() {
     int height = getmaxheight(), width = getmaxwidth();
     initwindow(SETTINGS_WIDTH, SETTINGS_HEIGHT, SETTINGS_TITLE,width/2 - SETTINGS_WIDTH/2, height/2 - SETTINGS_HEIGHT/2);
-    
+
     drawScreen();
     setTextSettings();
-    //drawButtons();
 
     MyArray<TextLabel, 3> labels;
     MyArray<TextButton,3> rectButtons;
-    labels[0] = TextLabel(100, 100, 120, 40, "Label!");
-    labels[0].drawTextLabel();
+    MyArray<char, 128> text = "Tema / Theme";
+    const char* p = text.data();
+    labels[0] = TextLabel(100, 50, 140, 50, LABEL_COLOR, p);
+    labels[0].drawTextLabel(FONT,FONT_SIZE);
+    text = "Limba/Language";
+    labels[1] = TextLabel(100, 250, 140, 50, LABEL_COLOR, p);
+    labels[1].drawTextLabel(FONT,FONT_SIZE);
 
-    rectButtons[0] = TextButton(500, 100, 120, 40,"Buton!");
-    rectButtons[0].drawTextButton();
+
+
+    text = "Dark";
+    rectButtons[0] = TextButton(500, 50, 140, 50,RGB(80,80,80),p);
+    text = "Light";
+    rectButtons[1] = TextButton(350,50,140,50,RGB(180,180,180),p);
+    text = "Start";
+    rectButtons[2] = TextButton(300,350,140,50,RGB(180,180,180),p);
+
+    for(size_t i = 0; i < rectButtons.size(); i++){
+        rectButtons[i].drawTextButton(FONT,FONT_SIZE);
+    }
+
+
+    text = "./media/RO.jpg";
+    ImageButton im = ImageButton(350,250,140,50,0,p);
+    im.drawImageButton();
+    text = "./media/EN.jpg";
+    ImageButton im2 = ImageButton(500,250,140,50,0,p);
+    im2.drawImageButton();
+
 
     do {
-        //sincer habar nu am cum o sa fac o singura functie
-        // care mi face check la TOATE TIPURILE de butoane
-        // daca ai tu vreo idee....
-        // gen, da, as putea sa fac un template function
-        // si sa mi l apelez de 3 ori pt cele 3 tipuri de butoane
-        // dar oare e eficient?
-        // daca ai o idee, sa mi zici
-        //checkHover(rectButtons);
-        //checkMouse(rectButtons);
+        //daca ai idei mai bune pentru un click checker decat un template apelat pt toate arrayurile, sa mi zici
     } while (true);
 }
