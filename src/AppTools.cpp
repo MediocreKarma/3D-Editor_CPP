c4e34557818390604d845cd7d2f1e3e521774878
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2022-11-26 04:06:49 +0200

    Add files via upload

--- src/AppTools.cpp
+++ src/AppTools.cpp
@@ -6,10 +6,8 @@ Label::Label() :
 Label::Label(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_) :
     xCenter(xCenter_), yCenter(yCenter_), xLen(xLen_), yLen(yLen_), fillColor(fillColor_){}
 
-void Label::drawLabel(const int& outlineColor) {
-    setcolor(outlineColor);
-    rectangle(xCenter - xLen / 2, yCenter - yLen / 2,
-        xCenter + xLen / 2, yCenter + yLen / 2);
+void Label::drawLabel(const int& outlineColor) const {
+    border(outlineColor);
     if (fillColor >= 0) {
         setfillstyle(SOLID_FILL, fillColor);
         bar(xCenter - xLen / 2 + 1, yCenter - yLen / 2 + 1,
@@ -17,16 +15,22 @@ void Label::drawLabel(const int& outlineColor) {
     }
 }
 
-void Label::clear(const int& barColor) {
+void Label::border(const int& outlineColor) const {
+    setcolor(outlineColor);
+    rectangle(xCenter - xLen / 2, yCenter - yLen / 2,
+        xCenter + xLen / 2, yCenter + yLen / 2);
+}
+
+void Label::clear(const int& barColor) const {
     setfillstyle(SOLID_FILL, barColor);
     bar(xCenter - xLen / 2, yCenter - yLen / 2,
         xCenter + xLen / 2, yCenter + yLen / 2);
 }
 
-TextLabel::TextLabel() {
-    m_text = "";
-}
-TextLabel::TextLabel(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_, const char*& text) :
+TextLabel::TextLabel() :
+    Label(), m_text() {}
+
+TextLabel::TextLabel(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_, const char* text) :
     Label(xCenter_, yCenter_, xLen_, yLen_, fillColor_), m_text(text){}
 
 void TextLabel::drawText(const int& txtFont, const int& txtSize) {
@@ -40,33 +44,26 @@ void TextLabel::drawTextLabel(const int& txtFont, const int& txtSize) {
     drawText(txtFont, txtSize);
 }
 
-
 Button::Button() = default;
 
 Button::Button(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fillColor_) :
     Label(xCenter_, yCenter_, xLen_, yLen_, fillColor_) {}
 
-bool Button::hitCollision(const int& x, const int& y) {
+bool Button::hitCollision(const int& x, const int& y) const {
     return xCenter - xLen / 2 <= x && x <= xCenter + xLen / 2 &&
         yCenter - yLen / 2 <= y && y <= yCenter + yLen / 2;
 }
 
+ImageButton::ImageButton() :
+    Button(), filename() {}
 
-ImageButton::ImageButton() {
-    filename = "";
-}
-ImageButton::ImageButton(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fC_, const char*& filename_) :
+ImageButton::ImageButton(const int& xCenter_, const int& yCenter_, const int& xLen_, const int& yLen_, const int& fC_, const char* filename_) :
     Button(xCenter_, yCenter_, xLen_, yLen_, fC_), filename(filename_) {}
 
 void ImageButton::drawImageButton() {
-
-    readimagefile(filename.data(), xCenter - xLen / 2, yCenter - yLen / 2, xCenter + xLen / 2, yCenter + yLen / 2);
-    rectangle(xCenter - xLen / 2, yCenter - yLen / 2,
-        xCenter + xLen / 2, yCenter + yLen / 2);
-
-
+    drawLabel();
+    readimagefile(filename.data(), xCenter - xLen / 2, yCenter - yLen / 2, xCenter + xLen / 2 - 1, yCenter + yLen / 2 - 1);
 }
-
 
 TextButton::TextButton() :
     Button(), m_text() {}
--- src/SettingsMenuInterface.cpp
+++ src/SettingsMenuInterface.cpp
@@ -13,71 +13,119 @@ void SettingsMenuInterface::setTextSettings() {
     settextstyle(FONT, 0, FONT_SIZE);
 
 }
-template<class T>void SettingsMenuInterface::checkClick(MyArray<T,16> arr) {
 
-    if (ismouseclick(WM_LBUTTONDOWN))
-        {
-            int x, y;
-            getmouseclick(WM_LBUTTONDOWN, x, y);
-            for (size_t i = 0; i < arr.size(); i++) {
-                /*if (arr[i].isClicked(x,y)) {
-                    std::cout << i << " " << x << " " << y<<"\n";*/
-                    //tre sa vad cum fac cu isClicked ca tu ai schimbat in collision am vazut.
-                    //idee: vreau sa fac templateul sa verifice daca i array de Dropdowns
-                    //si da, cred ca am sa fac un array pentru singurul dropdown ever
-                    //sau... pot sa fac pentru un singur element? dar daca tot ii template facut pt arrayuri...
-                    //plus poate mai adaugam setari
-                    //iar collisionul dropdownului sa returneze int (indexul elementului clickuit)
-                    //si nu doar bool
-                    //ca sa schimbam doar un parametru cu care vom apela o functie
-                    //eu asa consider
-                }
+MyArray<TextLabel, LABEL_SIZE> SettingsMenuInterface::initLabels() {
+    MyArray<TextLabel, LABEL_SIZE> labels = {
+        TextLabel(100, 50, 140, 50, LABEL_COLOR, "Tema / Theme"),
+        TextLabel(100, 250, 140, 50, LABEL_COLOR, "Limba / Language")
+    };
+    return labels;
+}
 
-        }
+MyArray<TextButton, TEXTBUTTON_SIZE> SettingsMenuInterface::initThemeButtons() {
+    MyArray<TextButton, TEXTBUTTON_SIZE> themeButtons = {
+        TextButton(500, 50, 140, 50, RGB(80, 80, 80), "Dark"),
+        TextButton(350, 50, 140, 50, RGB(180, 180, 180), "Light")
+    };
+    return themeButtons;
+}
 
+MyArray<ImageButton, FLAG_SIZE> SettingsMenuInterface::initImageButtons() {
+    MyArray<ImageButton, FLAG_SIZE> flagButtons = {
+        ImageButton(350, 250, 140, 50, 0, "./media/RO.jpg"),
+        ImageButton(500, 250, 140, 50, 0, "./media/EN.jpg")
+    };
+    return flagButtons;
+}
 
+DropdownButton<DROPDOWN_SIZE> SettingsMenuInterface::buildDropdownButton() {
+    DropdownButton<DROPDOWN_SIZE> ddButton(300, 100, 100, 50, LIGHTGRAY, "1280x720", 200);
+    ddButton.addOption("1920x1080", WHITE);
+    ddButton.addOption("1600x900", WHITE);
+    ddButton.addOption("1280x720", WHITE);
+    ddButton.addOption("1000x750", WHITE);
+    ddButton.addOption("800x600", WHITE);
+    return ddButton;
+}
+
+void SettingsMenuInterface::drawLabels(MyArray<TextLabel, LABEL_SIZE>& labels) {
+    for (size_t i = 0; i < labels.size(); ++i) {
+        labels[i].drawTextLabel(FONT, FONT_SIZE);
+    }
+}
+
+void SettingsMenuInterface::drawThemeButtons(MyArray<TextButton, TEXTBUTTON_SIZE>& themeButtons) {
+    for(size_t i = 0; i < themeButtons.size(); ++i){
+        themeButtons[i].drawTextButton(FONT,FONT_SIZE);
+    }
+}
+
+void SettingsMenuInterface::drawFlagButtons(MyArray<ImageButton, FLAG_SIZE>& flagButtons) {
+    for (size_t i = 0; i < flagButtons.size(); ++i) {
+        flagButtons[i].drawImageButton();
+    }
 }
 
 void SettingsMenuInterface::run() {
     int height = getmaxheight(), width = getmaxwidth();
-    initwindow(SETTINGS_WIDTH, SETTINGS_HEIGHT, SETTINGS_TITLE,width/2 - SETTINGS_WIDTH/2, height/2 - SETTINGS_HEIGHT/2);
-
+    initwindow(SETTINGS_WIDTH, SETTINGS_HEIGHT, "Settings", width / 2 - SETTINGS_WIDTH / 2, height / 2 - SETTINGS_HEIGHT / 2);
     drawScreen();
     setTextSettings();
+    MyArray<TextLabel, LABEL_SIZE> labels(initLabels());
+    MyArray<TextButton, TEXTBUTTON_SIZE> themeButtons(initThemeButtons());
+    MyArray<ImageButton, FLAG_SIZE> flagButtons(initImageButtons());
+    TextButton startButton(300, 350, 140, 50, RGB(180, 180, 180), "Start");
+    DropdownButton<DROPDOWN_SIZE> ddButton(buildDropdownButton());
+    drawLabels(labels);
+    settingsMenu(themeButtons, flagButtons, startButton, ddButton);
+}
 
-    MyArray<TextLabel, 3> labels;
-    MyArray<TextButton,3> rectButtons;
-    MyArray<char, 128> text = "Tema / Theme";
-    const char* p = text.data();
-    labels[0] = TextLabel(100, 50, 140, 50, LABEL_COLOR, p);
-    labels[0].drawTextLabel(FONT,FONT_SIZE);
-    text = "Limba/Language";
-    labels[1] = TextLabel(100, 250, 140, 50, LABEL_COLOR, p);
-    labels[1].drawTextLabel(FONT,FONT_SIZE);
+bool SettingsMenuInterface::settingsMenu(MyArray<TextButton, TEXTBUTTON_SIZE>& themeButtons, MyArray<ImageButton, FLAG_SIZE>& flagButtons,
+                                         TextButton& startButton,  DropdownButton<DROPDOWN_SIZE>& ddButton) {
+    drawThemeButtons(themeButtons);
+    drawFlagButtons(flagButtons);
+    startButton.drawTextButton(FONT, FONT_SIZE);
+    ddButton.drawTextButton(FONT, FONT_SIZE);
+    while (true) {
+        while (!ismouseclick(WM_LBUTTONDOWN));
+        int x, y;
+        getmouseclick(WM_LBUTTONDOWN, x, y);
+        if (startButton.hitCollision(x, y)) {
+            closegraph();
+            return 1;
+            // run app instance with parameters resX, resY, theme,
+        }
+        for (size_t i = 0; i < themeButtons.size(); ++i) {
+            if (themeButtons[i].hitCollision(x, y)) {
+                themeButtons[i].border(RED);
+                theme = i;
+                for (size_t j = 0; j < themeButtons.size(); ++j) {
+                    if (i == j) {
+                        continue;
+                    }
+                    themeButtons[j].border(BLACK);
+                }
+            }
+        }
+        for (size_t i = 0; i < flagButtons.size(); ++i) {
+            if (flagButtons[i].hitCollision(x, y)) {
+                flagButtons[i].border(RED);
+                language = i;
+                for (size_t j = 0; j < flagButtons.size(); ++j) {
+                    if (i == j) {
+                        continue;
+                    }
+                    flagButtons[j].border(BLACK);
+                }
+            }
+        }
+        if (ddButton.hitCollision(x, y)) {
+            ddButton.toggleVisibillity(BACKGROUND_COLOR, FONT, FONT_SIZE);
+        }
+        if (ddButton.isListVisible()) {
+            for (size_t i = 0; i < DROPDOWN_SIZE; ++i) {
 
-
-
-    text = "Dark";
-    rectButtons[0] = TextButton(500, 50, 140, 50,RGB(80,80,80),p);
-    text = "Light";
-    rectButtons[1] = TextButton(350,50,140,50,RGB(180,180,180),p);
-    text = "Start";
-    rectButtons[2] = TextButton(300,350,140,50,RGB(180,180,180),p);
-
-    for(size_t i = 0; i < rectButtons.size(); i++){
-        rectButtons[i].drawTextButton(FONT,FONT_SIZE);
+            }
+        }
     }
-
-
-    text = "./media/RO.jpg";
-    ImageButton im = ImageButton(350,250,140,50,0,p);
-    im.drawImageButton();
-    text = "./media/EN.jpg";
-    ImageButton im2 = ImageButton(500,250,140,50,0,p);
-    im2.drawImageButton();
-
-
-    do {
-        //daca ai idei mai bune pentru un click checker decat un template apelat pt toate arrayurile, sa mi zici
-    } while (true);
 }
