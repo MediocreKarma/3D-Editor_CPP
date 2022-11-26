0796787bb1e5de78afec1c53705f4be660f11a9c
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2022-11-26 04:06:10 +0200

    Add files via upload

--- include/SettingsMenuInterface.h
+++ include/SettingsMenuInterface.h
@@ -1,8 +1,9 @@
 #ifndef SETTINGSMENUINTERFACE_H
 #define SETTINGSMENUINTERFACE_H
 
-#include "../MyArray.h"
-#include "../MyVector.h"
+#include "MyArray.h"
+#include "MyVector.h"
+#include <AppTools.h>
 #include "graphics.h"
 #include "winbgim.h"
 
@@ -10,19 +11,34 @@ namespace SettingsMenuInterface
 {
     static const int SETTINGS_WIDTH = 600;
     static const int SETTINGS_HEIGHT = 400;
-    static const char* SETTINGS_TITLE = "Settings";
     static const int BACKGROUND_COLOR = RGB(200, 200, 200);
     static const int LABEL_COLOR = RGB(160, 160, 160);
     static const int BUTTON_COLOR = RGB(160, 60, 160);
-    static int TEXT_COLOR = BLACK;
-    static int LIMBA = 0;
-    static int FONT = 3;
-    static int FONT_SIZE = 1;
+    static const size_t LABEL_SIZE = 2;
+    static const size_t TEXTBUTTON_SIZE = 3;
+    static const size_t FLAG_SIZE = 2;
+    static const size_t DROPDOWN_SIZE = 5;
+    static const int FONT = 3;
+    static const int FONT_SIZE = 1;
+    static int resolutionX = 800;
+    static int resolutionY = 800;
+    static int theme = BLACK;
+    static int language = 0;
 
     void run();
     void drawScreen();
+    void createButtons();
     void setTextSettings();
     void checkClick();
+    MyArray<TextButton, TEXTBUTTON_SIZE> initThemeButtons();
+    MyArray<TextLabel, LABEL_SIZE> initLabels();
+    MyArray<ImageButton, FLAG_SIZE> initImageButtons();
+    DropdownButton<DROPDOWN_SIZE> buildDropdownButton();
+    void drawThemeButtons(MyArray<TextButton, TEXTBUTTON_SIZE>& txtButtons);
+    void drawLabels(MyArray<TextLabel, LABEL_SIZE>& labels);
+    void drawFlagButtons(MyArray<ImageButton, FLAG_SIZE>& flagButtons);
+    bool settingsMenu(MyArray<TextButton, TEXTBUTTON_SIZE>& txtButtons, MyArray<ImageButton, FLAG_SIZE>& flagButtons,
+                      TextButton& startButton, DropdownButton<DROPDOWN_SIZE>& ddButton);
 }
 
 #endif // SETTINGSMENUINTERFACE_H
