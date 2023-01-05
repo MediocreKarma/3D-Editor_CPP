9cf90e66910868588fbace2e5f782f3c0e61c4c4
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2023-01-05 19:45:12 +0200

    New Object Creator

--- include/ObjectCreator.h
+++ include/ObjectCreator.h
@@ -8,34 +8,8 @@
 #include "Space3D.h"
 #include "AppTools.h"
 #include "TextInput.h"
-
-class Layer {
-    public:
-        Layer();
-        Layer(const Layer& other);
-        Layer& operator = (const Layer& other);
-        Point2D& operator [] (const size_t& index);
-        int height() const;
-        bool empty() const;
-        size_t size() const;
-        Point2D center() const;
-        void changeHeight(const int& height);
-        void addPoint(const Point2D& point);
-        void removePoint(const Point2D& point);
-        void removePoint(const size_t& index);
-        void update();
-        void update(const size_t& index);
-        MyVector<CircularButton> renderButtons(const int& x0, const int& y0, const int& x1, const int& y1);
-        void renderButtons(const int& x0, const int& y0, const int& x1, const int& y1, MyVector<CircularButton>& existingButtons);
-        void moveRel(const int& x, const int& y);
-        void moveRel(const Point2D& rel);
-
-    private:
-        MyVector<Point2D> m_points;
-        MyVector<bool> m_updated;
-        int m_height;
-        Point2D m_center;
-};
+#include "MyMap.h"
+#include "MyHashMap.h"
 
 class ObjectCreator {
     public:
@@ -43,39 +17,52 @@ class ObjectCreator {
             NewPoint = 0, MovePoint, ConnectPoint, DeletePoint, CutLine
         };
         ObjectCreator(const int& theme = 0);
-        ObjectCreator(Mesh editedMesh, const int& theme = 0);
+        ObjectCreator(const Mesh& editedMesh, const int& theme = 0);
         ObjectCreator(const ObjectCreator& other);
         ObjectCreator& operator = (const ObjectCreator& other);
-        Mesh run();
-        void draw(bool update = true);
+        FixedMesh run();
+        void draw();
         const int& getCloseFlag() const;
 
     private:
+        int m_width;
+        int m_height;
         int m_theme;
-        MyVector<Layer> m_layers;
-        MyVector<TextButton> m_layerSelectButtons;
-        MyVector<MyVector<CircularButton>> m_layerPointButtons;
-        MyVector<MyVector<size_t>> m_layerAdjListIndex;
+
+        struct LayerInfo {
+            MyHashMap<FixedMesh::iterator_type, CircularButton> data;
+            Point2D centerPoint;
+
+            LayerInfo() :
+                data(), centerPoint() {}
+
+            void updateButtons(const int xCenter, const int yCenter);
+        };
+
+        MyMap<int, LayerInfo> m_layers;
+
+        struct LayerSelectButtonData {
+            TextButton txtButton;
+            MyMap<int, LayerInfo>::iterator it;
+
+            LayerSelectButtonData() :
+                txtButton(), it() {}
+        };
+
+        MyVector<LayerSelectButtonData> m_layerSelectButtons;
+        MyMap<int, LayerInfo>::iterator m_selectedLayer;
         Button m_addLayerButton;
-        MyVector<CircularButton> m_pointButtons;
         Button m_minimizedSpaceButton;
         MyArray<ImageButton, 5> m_toolButtons;
         Tool m_tool;
-        int x0;
-        int y0;
-        int x1;
-        int y1;
         int workX0;
         int workY0;
         int workX1;
         int workY1;
-        Space3D m_workArea;
-        Mesh* m_workMesh;
-        int m_selectedLayer;
-        int m_hovered;
+        FixedSpace3D m_workArea;
+        FixedMesh::iterator_type m_hovered;
         Line2D m_assistLine;
         bool m_assistLineDotted;
-        int m_layerSelectsBegin, m_layerSelectsEnd;
         MyArray<Button, 2> m_layerScrollArrows;
         MyArray<ImageButton, 1> m_layerTools; //yet to be used
         int m_closeFlag; //close w discard vs close w save
@@ -84,12 +71,12 @@ class ObjectCreator {
         void init();
         void resetLine();
         int getLayerByHeight(const int& height);
-        void layerViewMover(const int& x_, const int& y_);
-        void pointMover(const size_t& index);
-        void pointConnector2D(const size_t& index);
-        void pointConnector3D(const size_t& index);
-        void pointDeleter(const size_t& index);
-        void pointDeleter3D(const size_t& index);
+        void layerViewMover(int x, int y);
+        void pointMover();
+        void pointConnector2D();
+        void pointConnector3D();
+        void pointDeleter();
+        void pointDeleter3D();
         void pointAdder(const int& x, const int& y);
         void lineCutter2D(const int& x, const int& y);
         void lineCutter3D(const int& x, const int& y);
@@ -102,15 +89,17 @@ class ObjectCreator {
         void drawPointData();
         MyArray<char, 32> itoa(int x, const char* prefix = nullptr);
         int atoi(MyArray<char, 256>& arr);
+        bool getClickCommand(const int x, const int y);
         bool getClickCommand();
         bool getHoverCommand();
         bool getHoverCommand(const int& x, const int& y);
         bool getDoubleClickCommand();
-        void toolOperationOnPoint(const size_t& index);
+        void toolOperationOnPoint();
         void renderLayerSelectButtons();
         void updateLayerSelectsInterval();
-        bool moveLayerSelectsInterval(const int& delta);
-        void editLayer(const int& index);
+        bool moveLayerSelectsInterval(const int dir);
+        void centerLayerButton();
+        void editLayer(const int layerIndex);
         Mesh generateCube(const unsigned int& length_);
         Mesh generateCylinder(const unsigned int& height, const unsigned int& radius, const unsigned int& sides);
         Mesh generateCone(const unsigned int& height, const unsigned int& radius, const unsigned int& sides);
