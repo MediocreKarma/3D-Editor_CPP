a382ff8a0abed75942839ea12c13a318104ef82b
MediocreKarma <103333398+MediocreKarma@users.noreply.github.com>
2023-01-07 16:08:48 +0200

    Update ObjectCreator.cpp

--- src/ObjectCreator.cpp
+++ src/ObjectCreator.cpp
@@ -767,8 +767,7 @@ bool ObjectCreator::getDoubleClickCommand() {
     return false;
 }
 
-
-FixedMesh ObjectCreator::run() {
+Mesh ObjectCreator::run() {
     initwindow(m_width, m_height, "Object creator", 300, 25, false, false);
     setvisualpage(0);
     setactivepage(1);
@@ -783,7 +782,7 @@ FixedMesh ObjectCreator::run() {
         }
     }
     closegraph(getcurrentwindow());
-    return m_workArea.mesh();
+    return Mesh(m_workArea.mesh());
 }
 
 const int& ObjectCreator::getCloseFlag() const {
